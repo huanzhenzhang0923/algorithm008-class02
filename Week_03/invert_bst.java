bd0e0909493ddbbee7ec88899110cc10f89030a5
章桓臻 <1067009151@qq.com>
2020-05-03 10:59:56 -0400

    add invert bst

---
+++ Week_03/invert_bst.java
@@ -0,0 +1,18 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+public class invert_bst {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right= invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
